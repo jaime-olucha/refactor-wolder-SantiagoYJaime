3b2f8c9470fb54dcd3921e60f5cc2542ff7976ab
jaimeolucha <jaime.olucha@cuatroochenta.com>
2026-03-02 14:07:29 +0100

    feat/Create UI class implementing the interface to control the game's actions

---
+++ src/infraestructure/UI.ts
@@ -0,0 +1,50 @@
+import { IGameUI } from "../shared/IGameUI";
+import { GameState, LetterState } from "../shared/types";
+import { UI_CONFIG, ALL_STATE_CLASSES } from "./config/uiConfig";
+
+export class UI implements IGameUI {
+
+    private getCellElement(row: number, column: number): HTMLElement | null {
+        const rowElement = document.getElementById(`row_${row}`);
+        if (!rowElement) return null;
+
+        return rowElement.children[column] as HTMLElement;
+    }
+
+    private setElementState(element: HTMLElement | null, state: LetterState):void {
+        if(!element) return
+
+        element.classList.remove(...ALL_STATE_CLASSES);
+        const classToAdd = UI_CONFIG.CSS_CLASSES[state];
+        if(classToAdd) element.classList.add(classToAdd);
+    }
+
+    
+    drawLetter(row: number, column: number, letter: string): void {
+        const cell = this.getCellElement(row, column);
+        if (cell) cell.textContent = letter
+    }
+
+    deleteLetter(row: number, column: number): void {
+        const cell = this.getCellElement(row, column);
+        if (cell) cell.textContent = "";
+    }
+
+    changeCellState(row: number, column: number, state: LetterState): void {
+        const cell = this.getCellElement(row, column);
+        if (cell) this.setElementState(cell, state);
+    }
+
+    changeKeyState(key: string, state: LetterState): void {
+        const button = document.querySelector(`.key[value="${key}"]`) as HTMLButtonElement | null;
+        if (button) this.setElementState(button, state);
+    }
+
+    onGameOver(state: GameState): void {
+        const route = UI_CONFIG.ROUTES[state];
+
+        if (route) {
+            window.location.assign(route);
+        }
+    }
+}
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,2 +1,11 @@
-export type LetterState = 'CORRECT' | 'MISPLACED' | 'WRONG' | 'EMPTY';
-export type GameState = 'PLAYING' | 'WON' | 'LOST';
+export enum GameState {
+    PLAYING = 'PLAYING',
+    WON = 'WON',
+    LOST = 'LOST',
+}
+export enum LetterState {
+    CORRECT = 'CORRECT',
+    PRESENT = 'PRESENT',
+    ABSENT = 'ABSENT',
+    EMPTY = 'EMPTY'
+}
