2ab0545c3d34035705db2c0530c6e285ead02f68
sapantiXD <santirodillamarti@gmail.com>
2026-03-03 09:11:38 +0100

    refactor: move game constants to new structure

---
+++ src/shared/constants.ts
@@ -0,0 +1,10 @@
+export const MAX_WORD_SIZE:number = 5;
+export const MAX_ATTEMPTS:number = 6;
+export const VALID_KEYS: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N','Ñ','O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+// Se exportan las constantes para que puedan ser utilizadas en otros archivos del proyecto, como en Game.ts e Interface.ts.
+// Estas constantes definen el tamaño máximo de la palabra, el número máximo de intentos y las teclas válidas que se pueden presionar durante el juego.
+// Cualquier dev que busque configuración solo tiene un lugar donde mirar.
+// Lo que facilita el mantenimiento y la actualización de estas constantes en el futuro.
+
