3269b84002cde040e713b3912c1621753ff8fd9a
sapantiXD <santirodillamarti@gmail.com>
2026-03-04 12:21:40 +0100

    refactor: new structure

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,19 +1,20 @@
 export const MAX_WORD_SIZE:number = 5;
 export const MAX_ATTEMPTS:number = 6;
-export const VALID_KEYS: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N','Semicolon','O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+export const VALID_KEYS: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N','Ñ','O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 export const COMMANDS = {
     ENTER: 'ENTER',
     BACKSPACE: 'BACKSPACE'
 } as const
-/*
+
 export const WORDS_DB: string[] = [
     "JUEGO", "TALAR", "BAILE", "ANDAR", "MONTE", 
-    "PLAYA", "PLATA", "ARBOL", "QUESO", "NIÑOS"
+    "PLAYA", "PLATA", "ARBOL", "QUESO", "NIÑOS",
+    "CIELO", "MUNDO", "PIANO", "FRUTA", "LIBRO",
+    "SOÑAR", "COMER", "VIVIR", "SALIR", "VOLAR",
+    "VERDE", "DULCE", "BRAVO", "CLARO", "NOCHE"
 ];
-*/
-export const WORDS_DB: string[] = [
-    "NIÑOS"
-];
+
+
 
 
 // Se exportan las constantes para que puedan ser utilizadas en otros archivos del proyecto, como en Game.ts e Interface.ts.
